d608252d781f980170c6373d72f3d6766633da44
Muhammad Asfar <asfarrashid2713@gmail.com>
2026-04-28 15:48:57 +0500

    filter query set

--- test.py
+++ test.py
@@ -3,7 +3,5 @@ import pandas as pd
 df = pd.read_csv(r"D:\DataEngineering\Pandas\DataSets\Titanic-Dataset.csv")
 # print(df)
 
-# df = df[[["Pclass"] == 3 ] & [["Sex"] == 'male']]
-# print(df)
 
-df.info()
+print(df.loc[(df["Pclass"] == 3) & (df["Sex"] == "male")])
