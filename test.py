9e8ca5d28ba10274ce5ad88a1debbf99471a9d1b
MOIZ-RANA <148377134+MOIZ-RANA@users.noreply.github.com>
2026-06-23 14:39:17 +0500

    Added comments to test.py

--- test.py
+++ test.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 df = pd.read_csv(r"D:\DataEngineering\Pandas\DataSets\Titanic-Dataset.csv")
 # print(df)
-
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
+# WE have made changes to this file
 
 print(df.loc[(df["Pclass"] == 3) & (df["Sex"] == "male")])
